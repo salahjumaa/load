33d173919be4a6a627985b5c315ec222bb1fef08
salahjumaa <salahjumaa0@gmail.com>
2023-11-27 17:30:31 +0200

    edit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+    $(".form-check").click(function(){
+        $(this).toggleClass("check");
+      // jjj
+    })
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var swiper = new Swiper('.swiper-container', {
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+
+  });
